d53a1aee9b31fb58cecf8101e631e39f88f02059
max-piter <61771015+max-piter@users.noreply.github.com>
2022-03-05 15:23:19 +0300

    1. Quartz [#175122]

--- src/test/java/ru/job4j/quartz/AlertRabbitTest.java
+++ src/test/java/ru/job4j/quartz/AlertRabbitTest.java
@@ -8,7 +8,6 @@ import java.io.PrintWriter;
 
 public class AlertRabbitTest {
 
-
     @Test
     public void whenLoadConfigSuccess() throws Exception {
         File source = new File("/Users/a123/projects/job4j_grabber/src/test/test.properties");
@@ -18,11 +17,11 @@ public class AlertRabbitTest {
             out.println("line3=3");
         }
 
-        int interval = Integer.parseInt(AlertRabbit.loadConfig("test.properties").getProperty("line")
-                + AlertRabbit.loadConfig("test.properties").getProperty("line3"));
+        int interval = Integer.parseInt(AlertRabbit.loadConfig("test.properties")
+                .getProperty("line")
+                + AlertRabbit.loadConfig("test.properties").
+                getProperty("line3"));
         Assert.assertEquals(interval, 13);
 
-
     }
-
 }
