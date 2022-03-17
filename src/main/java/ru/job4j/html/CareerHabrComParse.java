b49d50f976c8c610c80507151868e7fc8e63200b
max-piter <61771015+max-piter@users.noreply.github.com>
2022-03-17 17:36:39 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#285210]

--- src/main/java/ru/job4j/html/CareerHabrComParse.java
+++ src/main/java/ru/job4j/html/CareerHabrComParse.java
@@ -12,17 +12,20 @@ import ru.job4j.grabber.utils.CareerHabrDateTimeParser;
  */
 public class CareerHabrComParse {
     public static void main(String[] args) throws Exception {
-        Document doc = Jsoup
-                .connect("https://career.habr.com/vacancies/java_developer")
-                .get();
-        Elements row = doc.select(".vacancy-card__inner");
+        for (int i = 1; i <= 5; i++) {
+            String page = String.valueOf(i);
+            Document doc = Jsoup
+                    .connect("https://career.habr.com/vacancies/java_developer?page=" + page)
+                    .get();
+            Elements row = doc.select(".vacancy-card__inner");
 
-        for (Element el : row) {
-            CareerHabrDateTimeParser parser = new CareerHabrDateTimeParser();
-            System.out.println("https://career.habr.com" + el.children().get(1).attr("href"));
-            System.out.println(el.children().get(2).text());
-            System.out.println(parser
-                    .parse(el.children().get(0).children().get(0).attr("datetime")));
+            for (Element el : row) {
+                CareerHabrDateTimeParser parser = new CareerHabrDateTimeParser();
+                System.out.println("https://career.habr.com" + el.children().get(1).attr("href"));
+                System.out.println(el.children().get(2).text());
+                System.out.println(parser
+                        .parse(el.children().get(0).children().get(0).attr("datetime")));
+            }
         }
 
     }
