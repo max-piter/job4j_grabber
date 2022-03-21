68cdf51f4eb71c45b70b2652281e18c35eef37c5
max-piter <61771015+max-piter@users.noreply.github.com>
2022-03-21 10:45:34 +0300

    2.3. Загрузка деталей поста. [#285212 #277578]

---
+++ src/main/java/ru/job4j/html/VacancyDescription.java
@@ -0,0 +1,17 @@
+package ru.job4j.html;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+public class VacancyDescription {
+    private String retrieveDescription(String link) throws Exception {
+
+        Document doc =  Jsoup
+                .connect(link)
+                .get();
+
+        Elements divElement = doc.getElementsByAttributeValue("class", "style-ugc");
+        return divElement.text();
+    }
+}
